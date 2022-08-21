3fa901d63b7ec2c2d3ba0dc4c95ef6b94d5c2f2d
nitish <nitishpatnaik15@gmail.com>
2022-08-21 15:23:19 +0530

    Added dice image

---
+++ app/src/main/java/com/nitish/diceroller/Dice.kt
@@ -0,0 +1,13 @@
+package com.nitish.diceroller
+
+
+
+class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the result.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
